2fc12c349c1cccb9325507dee7636bb5d3865c6e
patrickkuchta <p.kuchta@mail.utoronto.ca>
2017-02-01 20:27:33 -0500

    poundsterlng

---
+++ inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * PoundSterling Theme Customizer
+ *
+ * @package PoundSterling
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function poundsterling_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'poundsterling_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function poundsterling_customize_preview_js() {
+	wp_enqueue_script( 'poundsterling_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'poundsterling_customize_preview_js' );
